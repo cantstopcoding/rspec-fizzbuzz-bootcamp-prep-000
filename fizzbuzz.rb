74bead06557d55a20dda9157ebd7452a72acb56d
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-02 23:52:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 
 def fizzbuzz(number)
  
- if number % 3 == 0
+ if number % 3 == 0 && number % 5 == 0 
+  return "FizzBuzz"
+ elsif number % 3 == 0
    return "Fizz"
  elsif number % 5 == 0
   return "Buzz"
- elsif number % 3 == 0 && number % 5 == 0 
-  return "FizzBuzz"
  end
 end 
